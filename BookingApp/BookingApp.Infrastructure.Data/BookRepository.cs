3fc96b885fb6c1e1707da5b48adb956c4be9a1ed
thanhdoan <thanh.doan1@rbi.co.uk>
2017-09-12 16:29:58 +0100

    Test

---
+++ BookingApp/BookingApp.Infrastructure.Data/BookRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using BookingApp.Domain.Entities;
+using BookingApp.Domain.Interfaces;
+
+namespace BookingApp.Infrastructure.Data
+{
+    public class BookRepository : IBookRepository
+    {
+        public BookRepository()
+        {
+
+        }
+
+        public IEnumerable<Book> GetAllBook()
+        {
+            return new List<Book>()
+            {
+                new Book()
+                {
+                    Author = "A",
+                    Title = "A",
+                    Price = 12.0d,
+                    PublishedYear = 2017,
+                },
+                new Book()
+                {
+                    Author = "B",
+                    Title = "B",
+                    Price = 12.0d,
+                    PublishedYear = 2017,
+                }, new Book()
+                {
+                    Author = "C",
+                    Title = "C",
+                    Price = 12.0d,
+                    PublishedYear = 2017,
+                }
+                , new Book()
+                {
+                    Author = "D",
+                    Title = "D",
+                    Price = 12.0d,
+                    PublishedYear = 2017,
+                }
+            };
+        }
+    }
+}
